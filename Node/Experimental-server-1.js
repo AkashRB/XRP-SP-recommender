aa26015899462256bd2b73ebb44951ddec111797
AkashRB <akashrrb@gmail,com>
2017-07-25 20:16:19 +0530

    Added the Access control

--- Node/Experimental-server-1.js
+++ Node/Experimental-server-1.js
@@ -1,21 +1,28 @@
 
 var express = require('express'),
-     http = require('http');
+  http = require('http');
 
 var hostname = 'localhost';
 var port = 1111;
 
 var app = express();
+var router = express.Router();
 
-app.get('/history', function (req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ 'a': 1 }));
-    //res.end('This will show you the buying history');
-});
-  
+    //BasketHandler Routes
+    router.route('/history').get(function (req, res) { res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1111');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.send(JSON.stringify({ 'a': 1 })); });
+
+
+
 app.use(function (req, res, next) {
   console.log(req.headers);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   //res.end('<html><body><h1>Hello World</h1></body></html>');
 
 });
@@ -23,7 +30,7 @@ app.use(function (req, res, next) {
 
 var server = http.createServer(app);
 
-server.listen(port, hostname, function(){
+server.listen(port, hostname, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
