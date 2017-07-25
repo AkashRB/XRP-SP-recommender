aefd1faf3f64c844fd3805858917d25f80855214
AkashRB <akashrrb@gmail,com>
2017-07-25 20:18:18 +0530

    changes the response to JSON

--- Node/Experimental-server-1.js
+++ Node/Experimental-server-1.js
@@ -8,23 +8,24 @@ var port = 1111;
 var app = express();
 var router = express.Router();
 
-    //BasketHandler Routes
-    router.route('/history').get(function (req, res) { res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1111');
+app.use(function (req, res, next) {
+  console.log(req.headers);
+  
+  //res.end('<html><body><h1>Hello World</h1></body></html>');
+  next();
+
+});
+
+app.get('/history',function (req, res) {
+  console.log("Inside the route history:");
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin','*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.send(JSON.stringify({ 'a': 1 })); });
-
-
-
-app.use(function (req, res, next) {
-  console.log(req.headers);
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  //res.end('<html><body><h1>Hello World</h1></body></html>');
-
+  res.send(JSON.stringify({ 'a': 1 }));
 });
 
 
